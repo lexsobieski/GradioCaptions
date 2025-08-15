3f23678a9db8aa17e4ff7b6c93b420938db67342
Maryna Kosse <mkosse@kse.org.ua>
2025-08-15 20:07:12 +0300

    Fixed save

--- caption_editor_functions.py
+++ caption_editor_functions.py
@@ -1,24 +1,39 @@
 import pandas as pd
-
-with open("Resources/captions.jsonl") as file:
-    captions = pd.read_json(file, lines=True)
+import numpy as np
 
 
 def get_captions_by_video_id(video_id):
-    global captions
+    with open("Resources/captions.jsonl") as file:
+        captions = pd.read_json(file, lines=True)
+
     captions_edit = captions[captions['file'] == video_id]
     captions_edit = captions_edit[['start_time', 'text', 'end_time']]
     captions_edit.columns = ["Start", "Text", "End"]
     return captions_edit
 
 
-def save(df):
+def save_dataframe(df, video_id, user):
     try:
-        global captions
-        captions['start_time'] = df['Start'].apply(lambda x: float(x))
-        captions['text'] = df['Text']
-        captions['end_time'] = df['End'].apply(lambda x: float(x))
-        captions.to_json('Resources/captions2.jsonl', orient='records', lines=True)
+        with open("Resources/captions.jsonl") as file:
+            captions = pd.read_json(file, lines=True)
+
+        other_captions = captions[captions['file'] != video_id].copy()
+        new_captions = captions[captions['file'] == video_id].copy()
+
+        new_captions['start_time'] = np.where(df['Start'].isnull(),
+                                              new_captions['start_time'],
+                                              df['Start'].apply(lambda x: float(x)))
+        new_captions['text'] = np.where(df['Text'].isnull(),
+                                        new_captions['text'],
+                                        df['Text'])
+        new_captions['end_time'] = np.where(df['End'].isnull(),
+                                            new_captions['end_time'],
+                                            df['End'].apply(lambda x: float(x)))
+        new_captions['user_id'] = user
+
+        all_captions = pd.concat([other_captions, new_captions], ignore_index=True)
+
+        all_captions.to_json('Resources/captions.jsonl', orient='records', lines=True)
         return "Save successful!"
     except ValueError:
         return "Save failed: Incorrect input format"
